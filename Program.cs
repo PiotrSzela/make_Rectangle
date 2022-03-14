323d61081d5c93004ce306ec5418605764f5f733
Pan Prezes <Pan Prezes@PREZES>
2022-03-14 14:04:31 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace make_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the lenght: ");
+            string xs = Console.ReadLine();
+            Console.Write("Enter the height: ");
+            string ys = Console.ReadLine();
+            int x = int.Parse(xs);
+            int y = int.Parse(ys);
+            Console.WriteLine();
+
+            for (int i = 0; i < y; i++)
+            {
+                Console.WriteLine(" ");
+
+                    for (int j = 0; j < x; j++)
+                    {
+                    
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(" *");
+
+                    }
+                
+                Console.WriteLine();
+
+            }
+            Console.ResetColor();
+            Console.ReadKey();
+
+        }
+    }
+}
